5cec83cb8f918958726c0273b37dca7ec93ac28e
Aziel <azielfreitas@gmail.com>
2025-05-29 16:59:22 -0300

    Add table to dashboard from an import

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ sys.path.append("./scripts")
 sys.path.append("./scripts/fuelcomponents")
 sys.path.append("./scripts/fuelproject_tables")
 from fuelproject_tables import dfgen
-from fuelcomponents import year_slider, all_time_avg
+from fuelcomponents import year_slider, all_time_avg, city_overview
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.SLATE])
 app.layout = html.Div([
@@ -16,55 +16,33 @@ app.layout = html.Div([
     dbc.Row([
         dbc.Col([
             html.H4("Select a range in years"),
-            year_slider.chart(),
+            year_slider.chart("all_time_slider"),
             dcc.Graph(
                 id = "fuel_avg",
             ),
         ]),
-    ]),
-    #     dbc.Col([
-    #         html.H4("Select a range in years"),
-    #         dcc.RangeSlider(
-    #             id = "slider",
-    #             min = daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[0],
-    #             max = daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[-1],
-    #             value = [
-    #                     daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[-5],
-    #                     daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[-1]
-    #                     ],
-    #             step = 1,
-    #             marks = {i: str(i) for i in range(
-    #                         daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[0],
-    #                         daily_fuel_avg["Data da Coleta"].dt.year.unique().tolist()[-1],
-    #                         1)
-    #                     },
-    #         ),
-    #         dash_table.DataTable(
-    #             id = "tbl_out",
-    #             data = city_overall.to_dict('records'),
-    #             columns=[{"name": i, "id": i} for i in city_overall.columns],
-    #             sort_action="native",
-    #             page_size=10,
-    #             merge_duplicate_headers=True
-    #         )
-    #     ])
-    # ]),
-
-    dbc.Row([
         dbc.Col([
-            dcc.Graph(
-                id = "city_alltime_avg",
-                figure = px.bar(
-                    dfgen.all_time_avg().sort_values("Municipio",ascending=True),
-                    title = "City all-time average, all products",
-                    x = "Municipio",
-                    y = "Normalized",
-                    color = "Produto",
-                )
-            ),
-        ])
-    ])
+            html.H4("Select a range in years"),
+            year_slider.chart("slider"),
+            dbc.Table(id="city_summary_over_year")    # html.Div(id="cities_table") works too
+        ]),
+    ]),
+
+    # dbc.Row([
+    #     dbc.Col([
+    #         dcc.Graph(
+    #             id = "city_alltime_avg",
+    #             figure = px.bar(
+    #                 dfgen.all_time_avg().sort_values("Municipio",ascending=True),
+    #                 title = "City all-time average, all products",
+    #                 x = "Municipio",
+    #                 y = "Normalized",
+    #                 color = "Produto",
+    #             )
+    #         ),
+    #     ])
+    # ])
 ])
 
 if __name__ == "__main__":
-  app.run(jupyter_mode="external")
+  app.run()
