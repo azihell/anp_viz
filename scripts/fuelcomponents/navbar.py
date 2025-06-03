c9b40cc2e47962605a74ae42924ce10e707fc106
Aziel <azielfreitas@gmail.com>
2025-06-03 18:48:22 -0300

    Navbar improvements

--- scripts/fuelcomponents/navbar.py
+++ scripts/fuelcomponents/navbar.py
@@ -21,42 +21,52 @@ def generate_navbar():
             children=[
                 dbc.Row([
                     dbc.Col(
-                        dbc.Button("Open OffCanvas", 
+                        dbc.Button(
+                            children=[
+                                html.Img(
+                                    src="/assets/menu-bar-icon.png",
+                                   style={"max-height":"30px"},  # Adjust image height as needed
+                                    # className="me-2", # Add some margin to the right of the image
+                                ),
+                                # "Download", # You can still have text next to the image
+                            ],
                                 id="open-offcanvas-scrollable",
                                 n_clicks=0,
-                                outline=True, 
-                                color="light",
-                                className="ms-2",
+                                outline=False, 
+                                color="secondary",
+                                className="p-0 m-0",
+                                style={"border-radius":"10px"}
                                 ),
                     width="auto"),
-                    dbc.Col(dbc.NavbarBrand("Histórico de preços", className="ms-2")),
-                    ], align="center", className="g-2"
+                    dbc.Col(dbc.NavbarBrand("Histórico de preços", className="ms-2", style={"max-height":"30px"})),
+                    ], align="center", className="g-0"
                 ),
-                # dbc.NavItem(dbc.NavLink("Home", href="/")),
-                # dbc.NavItem(dbc.NavLink("Sobre", href="/about")),
-                # dbc.DropdownMenu(
-                #     children=[
-                #         dbc.DropdownMenuItem("More pages", header=True),
-                #         dbc.DropdownMenuItem("Page 1", href="/page1"),
-                #         dbc.DropdownMenuItem("Page 2", href="/page2"),
-                #     ],
-                #     nav=True,
-                #     in_navbar=True,
-                #     label="Mais",
-                #     align_end=True
-                # ),
+                dbc.NavItem(dbc.NavLink("Home", href="/")),
+                dbc.NavItem(dbc.NavLink("Sobre", href="/about")),
+                dbc.DropdownMenu(
+                    children=[
+                        dbc.DropdownMenuItem("More pages", header=True),
+                        dbc.DropdownMenuItem("Page 1", href="/page1"),
+                        dbc.DropdownMenuItem("Page 2", href="/page2"),
+                    ],
+                    nav=True,
+                    in_navbar=True,
+                    label="Mais",
+                    align_end=True
+                ),
             # offcanvas
             offcanvas.generate_offcanvas()
             ],
-        fluid=True),
+        fluid=True,
+        style={"min-height":"40px", "max-height":"40px"}
+        ),
         # brand="Combustíveis - histórico de preços",
         # brand_href="/",
         color="rgb(50, 56, 62)",
         dark=True,
         fixed="top",  # <<< This makes the navbar fixed at the top
         # className="mb-4", # Add margin-bottom for spacing below the navbar
-        # fluid=True
-    )
+    style={"max-height":"50px"} )
     return navbar
 
 @callback(
