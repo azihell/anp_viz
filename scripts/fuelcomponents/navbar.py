26db1bedac621a6815fcd1b7266409a4184bbfd5
Aziel <azielfreitas@gmail.com>
2025-06-03 18:10:06 -0300

    Fix Navbar elements position and add hamburger menu icon

--- scripts/fuelcomponents/navbar.py
+++ scripts/fuelcomponents/navbar.py
@@ -17,30 +17,38 @@ offcanvass = html.Div(
 
 def generate_navbar():
     navbar = dbc.Navbar(
-        children=[
-            dbc.Button("Open OffCanvas", 
-                       id="open-offcanvas-scrollable",
-                       n_clicks=0,
-                       outline=True, 
-                       color="light",
-                       className="me-1", 
-                       ),
-            dbc.NavItem(dbc.NavLink("Home", href="/")),
-            dbc.NavItem(dbc.NavLink("Sobre", href="/about")),
-            dbc.DropdownMenu(
-                children=[
-                    dbc.DropdownMenuItem("More pages", header=True),
-                    dbc.DropdownMenuItem("Page 1", href="/page1"),
-                    dbc.DropdownMenuItem("Page 2", href="/page2"),
-                ],
-                nav=True,
-                in_navbar=True,
-                label="Mais",
-                align_end=True
-            ),
-        # offcanvas
-        offcanvas.generate_offcanvas()
-        ],
+        dbc.Container(
+            children=[
+                dbc.Row([
+                    dbc.Col(
+                        dbc.Button("Open OffCanvas", 
+                                id="open-offcanvas-scrollable",
+                                n_clicks=0,
+                                outline=True, 
+                                color="light",
+                                className="ms-2",
+                                ),
+                    width="auto"),
+                    dbc.Col(dbc.NavbarBrand("Histórico de preços", className="ms-2")),
+                    ], align="center", className="g-2"
+                ),
+                # dbc.NavItem(dbc.NavLink("Home", href="/")),
+                # dbc.NavItem(dbc.NavLink("Sobre", href="/about")),
+                # dbc.DropdownMenu(
+                #     children=[
+                #         dbc.DropdownMenuItem("More pages", header=True),
+                #         dbc.DropdownMenuItem("Page 1", href="/page1"),
+                #         dbc.DropdownMenuItem("Page 2", href="/page2"),
+                #     ],
+                #     nav=True,
+                #     in_navbar=True,
+                #     label="Mais",
+                #     align_end=True
+                # ),
+            # offcanvas
+            offcanvas.generate_offcanvas()
+            ],
+        fluid=True),
         # brand="Combustíveis - histórico de preços",
         # brand_href="/",
         color="rgb(50, 56, 62)",
