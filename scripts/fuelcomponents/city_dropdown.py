6cd59cb424401c0526d8d79553a5a48fbc2f1a01
Aziel <azielfreitas@gmail.com>
2025-06-05 12:53:39 -0300

    Add Dropdown component module - city selector

---
+++ scripts/fuelcomponents/city_dropdown.py
@@ -0,0 +1,15 @@
+from dash import dcc, html, callback, Output, Input, State
+import dash_bootstrap_components as dbc
+from fuelproject_tables import dfgen
+
+cities = dfgen.city_overall()["Municipio"].unique().tolist()
+
+def chart(referred_chart):
+    city_dropdown_list = dbc.Container([
+        dcc.Dropdown(
+            id = referred_chart,
+            options = cities,
+            value="Salvador"
+        ),
+    ])
+    return city_dropdown_list
